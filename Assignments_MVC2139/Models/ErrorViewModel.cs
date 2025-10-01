0dffa7e5cb9a14506dd66f38fa0c2a1da3b22561
Cheng Yeh Tsai <cheng-yeh.tsai@georgebrown.ca>
2025-10-01 15:33:26 -0400

    first commit

---
+++ Assignments_MVC2139/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Assignments_MVC2139.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
